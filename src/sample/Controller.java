d816cb575398220eb27386d68d986a3cecd895c7
David-Rodden <davidrodden273@gmail.com>
2017-06-02 01:13:52 -0700

    Reversed stack so that it expands from the bottom up and added scrolling to heap with bogus values

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -9,12 +10,19 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    private VBox stack;
+    private VBox stackBox;
     @FXML
-    private VBox heap;
+    private VBox heapBox;
+
+    private VMemory stack, heap;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        final VStack vStack = new VStack(stack);
+        stack = new VMemory(stackBox);
+        heap = new VMemory(heapBox, 50);
+        stack.push("ref: abc_int");
+        stack.push("ref: abc2_String");
+        stack.pop();
+        stack.push("ref: abc2_char");
     }
 }
